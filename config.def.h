e66c6f0d0e86cb8b9031fdc206f866af45c932d3
zulubit <zaanfr@gmail.com>
2025-01-06 13:43:20 +0100

    Snail patch and licenses

--- config.def.h
+++ config.def.h
@@ -39,6 +39,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "@|@",      snail },
 };
 
 /* monitors */
@@ -53,7 +54,7 @@ static const MonitorRule monrules[] = {
 	{ "eDP-1",    0.5f,  1,      2,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
 	*/
 	/* defaults */
-	{ NULL,       0.55f, 1,      1,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
+	{ NULL,       0.64f, 1,      1,    &layouts[0], WL_OUTPUT_TRANSFORM_NORMAL,   -1,  -1 },
 };
 
 /* keyboard */
@@ -190,6 +191,7 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                    XKB_KEY_f,          setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                    XKB_KEY_c,          setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
 	{ MODKEY,                    XKB_KEY_e,          togglefullscreen, {0} },
--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
+# Print a green message with space above and below
+echo -e "\n\e[32mStarting installation process in ~/dwlit...\e[0m\n"
+
 cd ~/dwlit
 
+# Remove config.h
+echo -e "\n\e[32mRemoving config.h...\e[0m\n"
 sudo rm config.h
 
+# Clean and install
+echo -e "\n\e[32mCleaning and installing dwlit...\e[0m\n"
 sudo make clean
-
 sudo make HOME_DIR=$(echo ~) install
 
+# Clean again after install
+echo -e "\n\e[32mCleaning after installation...\e[0m\n"
 sudo make clean
+
+# Change to dwlb directory
+echo -e "\n\e[32mChanging directory to ~/dwlit/dwlb...\e[0m\n"
+cd ~/dwlit/dwlb
+
+# Install dwlb
+echo -e "\n\e[32mInstalling dwlb...\e[0m\n"
+sudo make install clean
+
